7201d10b73afd92e92405bdf4891a9b977622456
13Characters_ <13.0character@gmail.com>
2025-07-08 13:50:32 -0700

    Added wallpaper changing functionality to screenshot.py (for Windows only)

--- screenshot.py
+++ screenshot.py
@@ -3,21 +3,29 @@ from datetime import datetime
 import math
 from pathlib import Path
 from playwright.sync_api import sync_playwright
+import sys
 
-LATITUDE = 33.646
-LONGITUDE = -117.843
+LATITUDE = sys.argv[0]
+LONGITUDE = sys.argv[1]
 now = math.floor(datetime.now().timestamp())
-url = Path('lake.html').absolute().as_uri()
+p = Path(__file__).parent
+url = (p / "lake.html").as_uri()
 url += f"?LATITUDE={LATITUDE}&LONGITUDE={LONGITUDE}&STARTING_TIME={now}"
 
 def run(playwright):
-  firefox = playwright.firefox
-  browser = firefox.launch()
-  page = browser.new_page()
+  browser = playwright.chromium.launch()
+  ctx = browser.new_context()
+  page = ctx.new_page()
   page.set_viewport_size({"width": 3840, "height": 2160})
   page.goto(url)
-  page.screenshot(path="lake.png")
+  page.screenshot(path=(p / "lake.png"))
   browser.close()
 
 with sync_playwright() as playwright:
     run(playwright)
+
+from sys import platform
+if platform in ["win32", "cygwin"]:
+   import ctypes
+   import os
+   ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.abspath(p / "lake.png"), 3)
