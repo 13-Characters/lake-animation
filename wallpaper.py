75958ffd63a46771398d69c9d4fce41e4c27e6b1
13Characters_ <13.0character@gmail.com>
2025-07-22 01:15:10 -0700

    Finish cloud code

--- wallpaper.py
+++ wallpaper.py
@@ -5,20 +5,25 @@ import sky_utils
 import json
 import xml.dom.minidom
 from math import pow, pi, sin, cos, tan
+import cairosvg
 
 HORIZON_Y = 1420
 IMAGE_WIDTH = 3840
 IMAGE_HEIGHT = 2560
 HORIZONTAL_FOV = math.pi / 3
 CAMERA_AZIMUTH = math.pi * 1.45
+EARTH_ROTATION_PERIOD = 86164.098904
 
 now = math.floor(datetime.now().timestamp()) if len(sys.argv) <= 3 else float(sys.argv[3])
 
-with open("star.xml") as svg_file:
-    star_xml = xml.dom.minidom.parse(svg_file)
+with open("star.xml") as xml_file:
+    star_xml = xml.dom.minidom.parse(xml_file)
 
-with open("sun.xml") as svg_file:
-  sun_xml = xml.dom.minidom.parse(svg_file)
+with open("sun.xml") as xml_file:
+  sun_xml = xml.dom.minidom.parse(xml_file)
+
+with open("clouds.xml") as xml_file:
+  clouds_xml = xml.dom.minidom.parse(xml_file)
 
 with open("input.svg") as svg_file:
   sky_svg = xml.dom.minidom.parse(svg_file)
@@ -26,9 +31,10 @@ with open("input.svg") as svg_file:
 svg_element = sky_svg.childNodes[1]
 star_image = star_xml.firstChild
 sun = sun_xml.firstChild
+clouds = clouds_xml.firstChild
 
 def get_sun_color(sun_alt):
-  daytime = ((255, 255, 254), (255, 255, 255))
+  daytime = ((255, 255, 255), (255, 255, 255))
   sunset = ((255, 180, 104), (255, 173, 91))
   if sun_alt > 0.08 * math.pi:
     return daytime
@@ -38,27 +44,33 @@ def get_sun_color(sun_alt):
   result = [0, 0]
   for i in [0, 1]:
     result[i] = color_blend(sunset[i], daytime[i], t)
-  print(result)
   return result
 
 def place_sun(x, y, sun_color, sun_glow_color):
   sun_copy = sun.cloneNode(True)
   sun_copy.setAttribute("transform", f"translate({x} {y})")
-  sun_color = list(map(lambda x: str(round(x)), sun_color))
-  sun_glow_color = list(map(lambda x: str(round(x)), sun_glow_color))
   for i, element in enumerate(sun_copy.childNodes):
     if type(element) != xml.dom.minidom.Element:
       continue
     if i < 4:
-      element.setAttribute("style", f"display:inline;opacity:1;fill:rgb({' '.join(sun_glow_color)});fill-opacity:0.0538922;stroke:none;")
+      element.setAttribute("style", f"display:inline;opacity:1;fill:{color_to_hex_value(sun_glow_color)};fill-opacity:0.0538922;stroke:none;")
     else:
-      element.setAttribute("style", f"display:inline;fill:rgb({' '.join(sun_color)});fill-opacity:1;stroke:none;")
+      element.setAttribute("style", f"display:inline;fill:{color_to_hex_value(sun_color)};fill-opacity:1;stroke:none;")
   svg_element.appendChild(sun_copy)
 
 def place_star(x, y, scale):
   star_copy = star_image.cloneNode(True)
   star_copy.setAttribute("transform", f"translate({x} {y}) scale({scale})")
   svg_element.appendChild(star_copy)
+
+def place_clouds(x):
+  clouds_copy = clouds.cloneNode(True)
+  elements = [element for element in clouds_copy.childNodes if type(element) == xml.dom.minidom.Element]
+  pattern = elements[0]
+  rect = elements[1]
+  svg_element.appendChild(pattern)
+  rect.setAttribute("transform", f"translate({x} 667) scale(3.77913)")
+  svg_element.appendChild(rect)
 
 def save_svg():
   with open("output.svg", "w") as output:
@@ -110,6 +122,12 @@ def color_blend(color1, color2, t):
   blend = list(map(lambda x: x*255, blend))
   return blend
 
+def color_to_hex_value(color):
+  result = "#"
+  for channel in color:
+    result += f"{round(channel):02x}"
+  return result
+
 # Used to blend between the sunrise/sunset gradients
 def azimuth_blend(azimuth):
   threshold = math.pi / 9
@@ -146,8 +164,7 @@ def update_sky_colors(sun_azimuth, sun_altitude):
       # If only there were a general term for sunrise and sunset
       lowsun = color_blend(sunrise_colors[part], sunset_colors[part], t_azimuth)
       sky_color = color_blend(lowsun, daytime_colors[part], t_altitude)
-      sky_color = list(map(lambda x: str(round(x)), sky_color))
-      stops[i].setAttribute("style", f"stop-color:rgb({' '.join(sky_color)});stop-opacity:1;")
+      stops[i].setAttribute("style", f"stop-color:{color_to_hex_value(sky_color)};stop-opacity:1;")
   if (-0.08 * math.pi < sun_altitude <= 0):
     for i, part in enumerate(["top", "bottom"]):
       t_altitude = (sun_altitude - sunset_to_twilight_end[part]) / (sunset_to_twilight_start[part] - sunset_to_twilight_end[part])
@@ -157,8 +174,7 @@ def update_sky_colors(sun_azimuth, sun_altitude):
       lowsun = color_blend(sunrise_colors[part], sunset_colors[part], t_azimuth)
       twilight = color_blend(dawn_colors[part], dusk_colors[part], t_azimuth)
       sky_color = color_blend(twilight, lowsun, t_altitude)
-      sky_color = list(map(lambda x: str(round(x)), sky_color))
-      stops[i].setAttribute("style", f"stop-color:rgb({' '.join(sky_color)});stop-opacity:1;")
+      stops[i].setAttribute("style", f"stop-color:{color_to_hex_value(sky_color)};stop-opacity:1;")
   if (sun_altitude <= -0.08 * math.pi):
     for i, part in enumerate(["top", "bottom"]):
       t_altitude = (sun_altitude - twilight_to_night_end[part]) / (twilight_to_night_start[part] - twilight_to_night_end[part])
@@ -167,8 +183,7 @@ def update_sky_colors(sun_azimuth, sun_altitude):
       # If only there were a general term for sunrise and sunset
       twilight = color_blend(dawn_colors[part], dusk_colors[part], t_azimuth)
       sky_color = color_blend(night_colors[part], twilight, t_altitude)
-      sky_color = list(map(lambda x: str(round(x)), sky_color))
-      stops[i].setAttribute("style", f"stop-color:rgb({' '.join(sky_color)});stop-opacity:1;")
+      stops[i].setAttribute("style", f"stop-color:{color_to_hex_value(sky_color)};stop-opacity:1;")
 
 
 if __name__ == "__main__":
@@ -182,6 +197,7 @@ if __name__ == "__main__":
   if sun_XY:
     place_sun(*sun_XY, *sun_colors)
   update_sky_colors(*sun_az_alt)
+  
   if sun_az_alt[1] < 0:
     star_data = json.load(open("star_data.json"))
     for star in star_data:
@@ -191,4 +207,8 @@ if __name__ == "__main__":
       scale = (5 * pow(1.5, -mag)) - (6 * (1 - fade_out_function(sun_az_alt[1])))
       if star_XY and scale > 0:
         place_star(star_XY[0], star_XY[1], scale)
+
+  cloud_x = -((now/80000 % 1)+0.5)*7203.4177
+  place_clouds(cloud_x)
+  
   save_svg()
